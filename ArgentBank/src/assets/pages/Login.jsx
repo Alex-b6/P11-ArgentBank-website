e22f48bdc84dc60bfeeea40ebc7a19c13a6b4b37
AlexB6 <brou.alexandre.pro@gmail.com>
2024-06-10 16:09:37 +0200

    Add : Added dynamic login + user pages (SCSS included)

--- ArgentBank/src/assets/pages/Login.jsx
+++ ArgentBank/src/assets/pages/Login.jsx
@@ -1,9 +1,53 @@
-const Login = () => {
-    return (
-        <div>
-            TEST LOGIN
-        </div>
-    );
-};
+import '../styles/pages/login.scss';
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRememberMeChange = () => {
+    setRememberMe(!rememberMe);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //logique de soumission de formulaire ici
+  };
+
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" value={username} onChange={handleUsernameChange} />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" checked={rememberMe} onChange={handleRememberMeChange} />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button type="submit" className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
+  );
+}
 
 export default Login;
